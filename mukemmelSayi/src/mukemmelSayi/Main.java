813013d7f21b052286eb4e1768da099658c227f9
ahmet <ahmet@macs-imac>
2022-09-20 22:36:49 +0300

    Second commit

--- mukemmelSayi/src/mukemmelSayi/Main.java
+++ mukemmelSayi/src/mukemmelSayi/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// 6 --> 1,2,3
 		// 28 --> 1,2,4,7,14
-		int number = 6;
+		int number = 28;
 		boolean mukemmelMi = false;
 		int toplam=0;
 		
